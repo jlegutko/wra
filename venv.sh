a982529335ea5aefd844772e8fabe6f46764865f
Jadwiga Legutko <jlegutko3@gmail.com>
2018-06-03 14:49:50 +0200

    fix venv.sh

--- venv.sh
+++ venv.sh
@@ -3,7 +3,7 @@
 python3.5 -m venv --without-pip venv
 source venv/bin/activate
 curl https://bootstrap.pypa.io/get-pip.py | python
-pip install -r requirements.txt
+pip install -r requirements.txt --user
 deactivate
 
 exit 0
